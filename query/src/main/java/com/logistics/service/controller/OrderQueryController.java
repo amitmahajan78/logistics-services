62fca15aacf400aa393790f2acfb6f882cddacaa
Amit Mahajan <amitmahajan@post.com>
2020-03-26 13:03:10 +0000

    added list endpoint

--- query/src/main/java/com/logistics/service/controller/OrderQueryController.java
+++ query/src/main/java/com/logistics/service/controller/OrderQueryController.java
@@ -12,13 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import java.util.ArrayList;
 import java.util.UUID;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Slf4j
 @RestController
-@RequestMapping("v2/orders")
+@RequestMapping("v2/query/orders")
 @RequiredArgsConstructor
 public class OrderQueryController {
 
@@ -41,6 +42,38 @@ public class OrderQueryController {
 
         return new Orders((com.logistics.service.view.model.Orders) query1.getSingleResult(),
                 query2.getResultList());
+    }
+
+    // SELECT * FROM orders order by order_creation_time DESC LIMIT 10;
+
+    @RequestMapping(method = GET, value = "/list/{limit}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ArrayList getLatestOrdersWithLimit(@PathVariable int limit) {
+
+        // query order and bookings
+        Query query1 = entityManager.createQuery("SELECT o FROM Orders o order by o.orderCreationTime DESC");
+        query1.setMaxResults(limit);
+
+        ArrayList<Object> orderList = new ArrayList<>();
+
+        for (Object order : query1.getResultList()) {
+            com.logistics.service.view.model.Orders orders1 = (com.logistics.service.view.model.Orders) order;
+
+
+            // query order details
+            Query query2 = entityManager.createQuery("SELECT o FROM OrderDetails o " +
+                    "WHERE o.orderId = UUID(:orderId)");
+            query2.setParameter("orderId", orders1.getOrderId());
+
+            ArrayList<Object> orderDetailList = new ArrayList<>();
+            orderDetailList.add(orders1);
+            orderDetailList.add(query2.getResultList());
+
+            orderList.add(orderDetailList);
+
+        }
+
+
+        return orderList;
     }
 
 }
